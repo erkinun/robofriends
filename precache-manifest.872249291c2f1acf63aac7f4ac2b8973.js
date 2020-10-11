e66b1492a4a4b4823208c62173f92783000c18e5
Erkin <erkinun@gmail.com>
2020-10-11 17:31:42 +0100

    Updates

---
+++ precache-manifest.872249291c2f1acf63aac7f4ac2b8973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cdc7c4a42d6ae11544f5d8fa6f04de",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d83932f90209284918b1",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "607021de80b92cb77513",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d83932f90209284918b1",
+    "url": "/robofriends/static/js/2.e44ce9d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.e44ce9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607021de80b92cb77513",
+    "url": "/robofriends/static/js/main.88f6893c.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
